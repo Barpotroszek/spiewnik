f04dd581a5d56927395220122c8778ecf25de4c1
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2024-09-01 23:03:56 +0200

    Change displaying list
    Now it is being sorted by titles, not by artist like it was previousely.
    It will be easier to get, and ofc there was necessity to restructure
    data file. It has even some future, but now I see it may be useless

--- scripts/list-loader.js
+++ scripts/list-loader.js
@@ -1,41 +1,44 @@
+const combineTitles = (a, b) => {
+  if (a.title < b.title) return -1;
+  if (a.title > b.title) return -1;
+  return 0;
+};
 
 fetch("texts/data.json").then(async (e) => {
-  e.json().then((e) => {
+  e.json().then((data) => {
     const main = document.getElementsByTagName("main")[0];
 
     // Sorting
-    const keys = Object.keys(e).sort();
 
     // Adding links to page
-    
-    const url = new URL(window.location)
-    url.pathname='./song.html'
+
+    const url = new URL(window.location);
+    url.pathname = "./song.html";
 
     let even = false;
+    let ul = document.createElement("ul");
+    console.log({ data });
+    data.forEach((item) => {
+      // let t = document.createElement("h3");
+      // t.textContent = item;
+      // main.append(t);
+      // TODO: Make it work because it is not
+      // TODO: Rewrite the whole method
+      even = !even;
+      let li = document.createElement("li"),
+        a = document.createElement("a");
+      a.textContent = `${item["title"]} - ${item['artist']}`;
 
-    keys.forEach((key) => {
-      const v = e[key].sort();
-      let t = document.createElement("h3"),
-        ul = document.createElement("ul");
-      t.textContent = key;
-      main.append(t);
-      v.forEach((el) => {
-        even = !even;
-        let li = document.createElement("li"),
-          a = document.createElement("a");
-        a.textContent = el;
-
-        url.searchParams.set("artist", key)
-        url.searchParams.set("title", v)
-        a.setAttribute(
-          "href",
-          "song.html?artist=" + key + "&title=" + el + ".md"
-        );
-        even ? li.classList.add('even'):null;
-        li.append(a);
-        ul.append(li);
-      });
-      main.append(ul);
+      url.searchParams.set("artist", item["artist"]);
+      url.searchParams.set("title", item["title"]);
+      a.setAttribute(
+        "href",
+        "song.html?artist=" + item['artist'] + "&title=" + item["title"] + ".md"
+      );
+      even ? li.classList.add("even") : null;
+      li.append(a);
+      ul.append(li);
     });
+    main.append(ul);
   });
 });
